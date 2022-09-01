8ac90cad06c8b47fb9e4b1359b35cb7b766bd04c
je25th <je25th@kakao.com>
2022-09-01 22:31:30 +0900

    init commit

---
+++ src/test/java/com/iloveit/api/IloveitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iloveit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IloveitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
